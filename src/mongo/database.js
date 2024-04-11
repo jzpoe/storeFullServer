7b0e0d20424e229d2c515eea04afc9e858d2df4f
unknown <pmguez1287@gmail.com>
2024-04-11 09:51:30 -0500

    build3

--- src/mongo/database.js
+++ src/mongo/database.js
@@ -1,13 +1,14 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true, useUnifiedTopology: true
+console.log('Valor de MONGODB_URI:', process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log('Conexión a la base de datos exitosa');
-  }).catch((error) => {
+}).catch((error) => {
     console.error('Error al conectar a la base de datos:', error);
-
 });
 
 module.exports = mongoose
