4fbd1dfffee57d877dce4cce4b51648e5cd83dab
unknown <pmguez1287@gmail.com>
2024-04-11 14:47:00 -0500

    buil 12

--- src/mongo/database.js
+++ src/mongo/database.js
@@ -4,9 +4,8 @@ require('dotenv').config()
 
 const mongo= process.env.MONGODB_URI
 
-mongoose.connect(mongo, {
-    
-}).then(() => {
+mongoose.connect(mongo) 
+.then(() => {
     console.log('Conexión a la base de datos exitosa');
 }).catch((error) => {
     console.error('Error al conectar a la base de datos:', error);
